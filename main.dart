6aef4c0f3b60199138dc26d2f0002119cf2c3942
Souvik Giri <74680826+SouvikGiri@users.noreply.github.com>
2021-06-15 20:38:13 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,12 +1,18 @@
 import 'package:ECommerceApp/consts/theme_data.dart';
+import 'package:ECommerceApp/inner_screens/product_details.dart';
 import 'package:ECommerceApp/provider/dark_theme_provider.dart';
+import 'package:ECommerceApp/provider/products.dart';
 import 'package:ECommerceApp/screens/bottom_bar.dart';
 import 'package:ECommerceApp/screens/wishlist.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'inner_screens/brands_navigation_rail.dart';
+import 'inner_screens/categories_feeds.dart';
+import 'provider/cart_provider.dart';
+import 'provider/favs_provider.dart';
 import 'screens/cart.dart';
 import 'screens/feeds.dart';
+import 'screens/landing_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,14 +43,23 @@ class _MyAppState extends State<MyApp> {
         providers: [
           ChangeNotifierProvider(create: (_) {
             return themeChangeProvider;
-          })
+          }),
+          ChangeNotifierProvider(
+            create: (_) => Products(),
+          ),
+          ChangeNotifierProvider(
+            create: (_) => CartProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (_) => FavsProvider(),
+          ),
         ],
         child:
             Consumer<DarkThemeProvider>(builder: (context, themeData, child) {
           return MaterialApp(
             title: 'Flutter Demo',
             theme: Styles.themeData(themeChangeProvider.darkTheme, context),
-            home: BottomBarScreen(),
+            home: LandingPage(),
             //initialRoute: '/',
             routes: {
               //   '/': (ctx) => LandingPage(),
@@ -53,6 +68,8 @@ class _MyAppState extends State<MyApp> {
               CartScreen.routeName: (ctx) => CartScreen(),
               Feeds.routeName: (ctx) => Feeds(),
               WishlistScreen.routeName: (ctx) => WishlistScreen(),
+              ProductDetails.routeName: (ctx) => ProductDetails(),
+              CategoriesFeedsScreen.routeName: (ctx) => CategoriesFeedsScreen(),
             },
           );
         }));
